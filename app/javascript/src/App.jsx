6855d69c324e075c0d95e462de48e5dadfb37775
navaneethsdk <navaneethsdk@gmail.com>
2022-08-31 14:59:27 +0530

    Added slug to task

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 import { initializeLogger } from "common/logger";
+import Dashboard from "components/Dashboard";
+import PageLoader from "components/PageLoader";
 
 import { setAuthHeaders } from "./apis/axios";
 
@@ -16,7 +18,11 @@ const App = () => {
   }, []);
 
   if (loading) {
-    return <h1>Loading...</h1>;
+    return (
+      <div className="h-screen">
+        <PageLoader />
+      </div>
+    );
   }
 
   return (
@@ -24,6 +30,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" render={() => <div>Home</div>} />
         <Route exact path="/about" render={() => <div>About</div>} />
+        <Route exact component={Dashboard} path="/dashboard" />
       </Switch>
     </Router>
   );
--- config/webpack/alias.js
+++ config/webpack/alias.js
@@ -1,10 +1,9 @@
 module.exports = {
-    resolve: {
-      alias: {
-        apis: "src/apis",
-        common: "src/common",
-        components: "src/components",
-      },
+  resolve: {
+    alias: {
+      apis: "src/apis",
+      common: "src/common",
+      components: "src/components",
     },
-  };
-  
+  },
+};
